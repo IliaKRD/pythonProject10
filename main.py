36d5fef2ba8f67e5f25a24ebed0843eed6d8072e
AndreyMoh <130593747+IliaKRD@users.noreply.github.com>
2023-04-19 20:32:15 +0300

    35

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaa
-fdgdggggggggggggggggdfgdfgdfg
-dfffffffffffffffff
-def test():
-    print()
+import sqlite3 as sq
+
+with sq.connect("test.db") as con:
+    cur=con.cursor()
+    cur.execute("""CREATE TABLE users (name TEXT, sex INTEGER, old INTEGER, score INTEGER)""")
+    con.commit()
